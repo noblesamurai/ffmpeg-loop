ce5b451b4f2a49bc5ed4c9c18998e0b642ab087c
Tim Allen <tim@noblesamurai.com>
2018-02-22 09:27:32 +1100

    Initial cut.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/**
+ * @module ffmpeg-loop
+ */
+
+const ffmpeg = require('fluent-ffmpeg');
+const assert = require('assert');
+ffmpeg.setFfmpegPath(require('ffmpeg-static').path);
+
+/**
+ * Creates an infinitely looping readable stream from a video.
+ * @param {string} filename - path to video
+ * @param {integer} opts.fps
+ * @param {integer} opts.width
+ * @param {integer} opts.height
+ * @returns A fluent ffmpeg process - has pipe() method.
+ */
+module.exports = function (filename, opts) {
+  ['height', 'width', 'fps'].forEach((key) => {
+    assert(!isNaN(opts[key]), `${key} should be number - got ${opts[key]}`);
+  });
+  return ffmpeg(filename)
+    .inputOption('-stream_loop', -1)
+    .outputFormat('rawvideo')
+    .outputOption('-vcodec', 'rawvideo')
+    .outputOption('-pix_fmt', 'rgba')
+    .outputOption('-s', `${opts.width}x${opts.height}`)
+    .outputOption('-r', opts.fps);
+};
