78dabac6fa35ac10444a7f655b22adff8b629151
Tim Allen <tim@noblesamurai.com>
2020-03-18 11:46:50 +1100

    Allows you to not loop.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
  * @module ffmpeg-loop
  */
 
+const cropFilter = require('./crop');
 const ffmpeg = require('fluent-ffmpeg');
-const assert = require('assert');
 const last = require('lodash.last');
+const ow = require('ow');
+
 ffmpeg.setFfmpegPath(require('ffmpeg-static').path);
 
 /**
@@ -26,57 +28,41 @@ ffmpeg.setFfmpegPath(require('ffmpeg-static').path);
  * @returns A fluent ffmpeg process - has pipe() method.
  */
 module.exports = function (filename, opts) {
-  ['height', 'width', 'fps'].forEach(key => {
-    assert(!isNaN(opts[key]), `${key} should be number - got ${opts[key]}`);
-  });
-  const { start = 0 } = opts;
-  const filters = [
+  ow(opts, ow.object.partialShape({
+    fps: ow.number.integer,
+    height: ow.number.integer,
+    width: ow.number.integer
+  }));
+  const { fps, height, loop = true, start = 0, width } = opts;
+  const filters = loop ? [
     { filter: 'concat', options: { n: 2, v: 1, a: 0 }, outputs: 'concat' },
     {
       filter: 'setpts',
       inputs: 'concat',
       options: 'N/(FRAME_RATE*TB)'
     }
-  ];
+  ] : [];
   const command = ffmpeg()
     // Using -ss and -stream_loop together does not work well, so we have a
     // single non-looped version first to seek on.
     .input(filename)
-    .inputOption('-ss', start)
-    .input(filename)
-    .inputOption('-stream_loop', -1)
+    .inputOption('-ss', start);
+  if (loop) command.input(filename).inputOption('-stream_loop', -1);
+  command
     .noAudio()
     .outputFormat('rawvideo')
     .outputOption('-vcodec', 'rawvideo')
     .outputOption('-pix_fmt', 'rgba')
-    .outputOption('-s', `${opts.width}x${opts.height}`)
-    .outputOption('-r', opts.fps);
-  applyCrop(filters, opts);
-  command.complexFilter(filters);
+    .outputOption('-s', `${width}x${height}`)
+    .outputOption('-r', fps);
+  const crop = cropFilter(opts);
+  if (crop) {
+    if (filters.length) {
+      last(filters).outputs = 'toCrop';
+      crop.inputs = 'toCrop';
+    }
+    filters.push(crop);
+  }
+  if (filters.length) command.complexFilter(filters);
   return command;
 };
-
-/**
- * Apply a crop filter (if required) to the ffmpeg command based on provided opts.
- * All crop dimensions are for the original video size (not the output size).
- *
- * @private
- * @param {Array<string>} filters  The filters array - will be modified in place if applicable
- * @param {integer} opts.cropWidth - crop width (width and height are required).
- * @param {integer} opts.cropHeight - crop height
- * @param {integer} opts.cropX - crop x (x and y are optional. If not set, the
- *   default is the center position of the video).
- * @param {integer} opts.cropY - crop y
- */
-function applyCrop (filters, opts) {
-  const { cropWidth, cropHeight, cropX, cropY } = opts;
-  if (!cropWidth || isNaN(cropWidth) || !cropHeight || isNaN(cropHeight)) { return; }
-  const crop = [cropWidth, cropHeight];
-  if (!isNaN(cropX) && !isNaN(cropY)) crop.push(cropX, cropY);
-  last(filters).outputs = 'toCrop';
-  return filters.push({
-    filter: 'crop',
-    inputs: 'toCrop',
-    options: crop.join(':')
-  });
-}
